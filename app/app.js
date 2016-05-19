ec1de6c1eb77f27b0132247df00f09dc8b1146d4
Paul <wip321@yahoo.com>
2016-05-19 09:57:32 -0500

    task runners added.

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 var app = angular.module("TodoApp", []);
 app.controller('NavCtrl', function($scope){
   $scope.navItems = [{
+    name: "Logout"
+  },{
     name: "All Items"
   },{
     name: "New Item"
-  }]
-})
+  }];
+});
 app.controller("TodoCtrl", function($scope) {
   $scope.welcome = "hello";
 });
