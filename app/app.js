0b3cf250bdd6f7f7465d18952ac3a9851ebcb707
Paul <wip321@yahoo.com>
2016-05-21 10:36:16 -0500

    form added

--- app/app.js
+++ app/app.js
@@ -10,7 +10,38 @@ app.controller('NavCtrl', function($scope){
 });
 app.controller("TodoCtrl", function($scope) {
   $scope.welcome = "hello";
-  $scope.showListView = true;
+  $scope.showListView = false;
+  $scope.newTask = {}
+  $scope.items = [
+  {
+    id: 0,
+    task: "mow",
+    isCompleted: false,
+    dueDate: "NOW",
+    assignedTo: "greg",
+    urgency: "high",
+    location: "here",
+    dependencies: ["stuff", "more stuff"]
+  },{
+    id: 1,
+    task: "grade quizes",
+    isCompleted: false,
+    dueDate: "NOW",
+    assignedTo: "joe",
+    urgency: "high",
+    location: "NSS",
+    dependencies: ["wifi", "vodka", "laptop"]
+  },{
+    id: 2,
+    task: "sleep",
+    isCompleted: false,
+    dueDate: "soon",
+    assignedTo: "me",
+    urgency: "medium",
+    location: "home",
+    dependencies: ["dog", "bed", "pillows"]
+  }];
+
   $scope.newItem = function() {
     console.log("new item");
     $scope.showListView = false;
