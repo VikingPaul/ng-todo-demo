41073548eee00b85a0924972b03e7eac582cb526
Paul <wip321@yahoo.com>
2016-05-25 18:04:28 -0500

    factories

--- app/controlers/itemViewCtrl.js
+++ app/controlers/itemViewCtrl.js
@@ -1,18 +1,15 @@
-app.controller("itemViewCtrl", function($scope, $http,$routeParams) {
+app.controller("itemViewCtrl", function($scope, $http,$routeParams, itemStorage) {
   $scope.selectedItem = {};
   $scope.items = [];
   // $http.get(`https://viking-todo-app.firebaseio.com/things/${$routeParams.itemId}.json`)
   //   .success(function(items) {
   //     $scope.selectedItem = items
   // });
-  $http.get("https://viking-todo-app.firebaseio.com/things.json")
-    .success(function(items) {
-      Object.keys(items).forEach(function(key) {
-        items[key].id = key;
-        $scope.items.push(items[key]);
-        $scope.selectedItem = $scope.items.filter(function(item) {
-          return item.id === $routeParams.itemId;
-        })[0];
-    });
-  });
+
+  itemStorage.getItemList().then(function(itemCollection) {
+    $scope.items = itemCollection;
+    $scope.selectedItem = $scope.tiems.filter(function(item) {
+      return item.id === $routeParams.itemId;
+    })[0]
+  })
 });
